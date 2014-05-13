9010c51791675ba363e7ea92fc090fdd6a369c45
Alexis de Lattre <alexis@via.ecp.fr>
2014-05-14 00:25:53 +0200

    PEP8 updates Add support for account move generation on mass transfer validation Add "Back to draft" on validated mass transfers

--- donation_stay/wizard/create_donation_stay.py
+++ donation_stay/wizard/create_donation_stay.py
@@ -2,7 +2,8 @@
 ##############################################################################
 #
 #    Donation Stay module for OpenERP
-#    Copyright (C) 2014 Artisanat Monastique de Provence (http://www.barroux.org)
+#    Copyright (C) 2014 Artisanat Monastique de Provence
+#       (http://www.barroux.org)
 #    @author: Brother Irénée
 #
 #    This program is free software: you can redistribute it and/or modify
@@ -23,8 +24,6 @@
 from openerp.osv import orm, fields
 import openerp.addons.decimal_precision as dp
 from openerp.tools.translate import _
-from openerp.tools import DEFAULT_SERVER_DATE_FORMAT, DEFAULT_SERVER_DATETIME_FORMAT
-from datetime import datetime
 
 
 class donation_stay_create(orm.TransientModel):
@@ -41,51 +40,50 @@ class donation_stay_create(orm.TransientModel):
         }
 
     _defaults = {
-        'date_donation': fields.date.context_today, #default date: today
+        'date_donation': fields.date.context_today,  # default date: today
         }
 
-# 1. create object "donation.donation" (in database !), parameters are initialized in a "prepare function"
-#(only values which need values by default)
-
+    # 1. create object "donation.donation" (in database !),
+    # parameters are initialized in a "prepare function"
+    # (only values which need values by default)
     def _prepare_donation(self, cr, uid, stay, wizard, context=None):
-        
+
         campaign_model, campaign_id = \
             self.pool['ir.model.data'].get_object_reference(
                 cr, uid, 'donation_stay', 'stay_campaign')
         assert campaign_model == 'donation.campaign'
 
-        # here we obtain in a tupple (model,res_id) values memorized in the table "ir.model.data" ( stay origin: xml object)
-        
+        # here we obtain in a tupple (model,res_id) values memorized
+        # in the table "ir.model.data" (stay origin: xml object)
         product_model, stay_donation_product_id = \
             self.pool['ir.model.data'].get_object_reference(
-                cr, uid, 'donation_stay', 'product_product_stay_donation') #nom du module et nom objet xml en arguments
-        # "product_product_stay_donation": it's xml name for object "product_data"
-        
-        assert product_model == 'product.product', 'Wrong model'    # check model, assign default value
-        
-        product=self.pool['product.product'].browse(cr, uid, stay_donation_product_id, context=context)
-        
+                cr, uid, 'donation_stay', 'product_product_stay_donation')
+        # "product_product_stay_donation":
+        # it's the xml name for object "product_data"
+
+        # check model, assign default value
+        assert product_model == 'product.product', 'Wrong model'
+
+        product = self.pool['product.product'].browse(
+            cr, uid, stay_donation_product_id, context=context)
+
         vals = {
-          
             'partner_id': stay.partner_id.id,
-            'tax_receipt_option':stay.partner_id.tax_receipt_option,
+            'tax_receipt_option': stay.partner_id.tax_receipt_option,
             'journal_id': wizard.journal_id.id,
             'check_total': wizard.amount,
             'donation_date': wizard.date_donation,
             'donation_campaign_id': campaign_id,
-            'line_ids': [(0, 0,{ 
-                        'product_id':stay_donation_product_id,
-                        'amount': wizard.amount,
-                        'tax_receipt_ok':product.tax_receipt_ok,
-                        })],
-
-        
+            'line_ids': [
+                (0, 0, {
+                    'product_id': stay_donation_product_id,
+                    'amount': wizard.amount,
+                    'tax_receipt_ok': product.tax_receipt_ok,
+                    })],
         }
         return vals
-    
-    
+
     def create_donation(self, cr, uid, ids, context=None):
-        print "ids=", ids
         assert len(ids) == 1, 'Only 1 ID for this wizard fonction'
         wizard = self.browse(cr, uid, ids[0], context=context)
         stay_id = context['active_id']
@@ -93,42 +91,31 @@ class donation_stay_create(orm.TransientModel):
 
         if not stay.partner_id:
             raise orm.except_orm(
-                    _('Error:'),
-                    _("This Partner is anonymous. You must create a real Partner.")
-                                )
+                _('Error:'),
+                _("This Partner is anonymous. You must create a real "
+                    "Partner."))
 
-# 1. create object "donation.donation" (in database !), parameters are initialized in a "prepare fonction"
-#(only values which need values by default), values stocked in "receipt_vals"
-# create function used one time, but two objects are created : donation object and donation_line object
-       
-        donation_vals = self._prepare_donation(cr, uid, stay, wizard, context=context)
-        id_donation = self.pool['donation.donation'].create(cr, uid, donation_vals, context=context)
-        
-        #------------------------------------------------------------
-        #action_model, donation_action_id = \
-        #    self.pool['ir.model.data'].get_object_reference(
-        #        cr, uid, 'donation', 'donation_action')
-        #assert action_model == 'ir.actions.act_window', 'Wrong action model'
-        #action = self.pool[action_model].read(cr, uid, donation_action_id, context=context)
-        #action.update({
-        #        'nodestroy': False,
-        #        'res_id': id_donation,
-        #        'view_mode': 'form,tree',
-        #        c)
-        #------------------------------------------------------------
-        
+        # 1. create object "donation.donation" (in database !),
+        # parameters are initialized in a "prepare fonction"
+        # (only values which need values by default),
+        # values stocked in "receipt_vals"
+        # create function used one time, but two objects are created :
+        # donation object and donation_line object
+        donation_vals = self._prepare_donation(
+            cr, uid, stay, wizard, context=context)
+        donation_id = self.pool['donation.donation'].create(
+            cr, uid, donation_vals, context=context)
+
         # launch an action in order to open a view type "form" (donation form)
-        
         action = {
-            'name': 'Donations',
+            'name': _('Donations'),
             'type': 'ir.actions.act_window',
             'res_model': 'donation.donation',
             'view_mode': 'form,tree',
             'view_type': 'form',
             'nodestroy': False,
             'target': 'current',
-            'res_id': id_donation,
+            'res_id': donation_id,
             'context': context,
-            
-                  }
+            }
         return action
