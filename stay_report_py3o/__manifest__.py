402c694b90f619b8ed9b3db252af9b17c2ee3817
Alexis de Lattre <alexis.delattre@akretion.com>
2018-04-09 19:15:28 +0200

    Add module stay_report_py3o

---
+++ stay_report_py3o/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Akretion France (www.akretion.com)
+# @author: Alexis de Lattre <alexis.delattre@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Stay Report Py3o',
+    'version': '10.0.1.0.0',
+    'category': 'Lodging',
+    'license': 'AGPL-3',
+    'summary': 'Replace Qweb report by Py3o report on stay module',
+    'author': 'Barroux Abbey, Akretion, Odoo Community Association (OCA)',
+    'website': 'http://www.barroux.org',
+    'depends': ['report_py3o', 'stay'],
+    'data': ['report.xml'],
+    'installable': True,
+}
