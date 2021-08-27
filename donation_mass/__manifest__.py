784c6f3c5608f3e4e38dd32e63562e24838cdde2
Alexis de Lattre <alexis.delattre@akretion.com>
2021-08-27 21:02:31 +0200

    [MIG] mass and donation_mass from v10 to v14
    Several small improvements:
    - icon/button from donations to mass requests
    - celebrant on res.partner is now a selection field with 2 options: internal or external
    - the income account on mass product is used when validating the mass journal (in v10, we had to configure the stock account there, which was not good)
    - the stock account is now configured on the invoicing configuration page (company wide setting)

--- donation_mass/__manifest__.py
+++ donation_mass/__manifest__.py
@@ -1,24 +1,23 @@
-# -*- coding: utf-8 -*-
-# © 2014-2017 Barroux Abbey (www.barroux.org)
-# © 2014-2017 Akretion France (www.akretion.com)
+# Copyright 2014-2021 Barroux Abbey (www.barroux.org)
+# Copyright 2014-2021 Akretion France (www.akretion.com)
 # @author Alexis de Lattre <alexis.delattre@akretion.com>
 
 {
     'name': 'Donation Mass',
-    'version': '10.0.1.0.0',
+    'version': '14.0.1.0.0',
     'category': 'Religion',
     'license': 'AGPL-3',
     'summary': 'Ability to create mass from donation lines',
     'author': 'Barroux Abbey, Akretion, Odoo Community Association (OCA)',
-    'website': 'http://www.barroux.org',
+    'maintainers': ['alexis-via'],
+    'website': 'https://github.com/OCA/vertical-abbey',
     'depends': ['donation', 'mass'],
     'data': [
-        'mass_view.xml',
-        'donation_view.xml',
-        'donation_mass_data.xml',
-        'wizard/select_mass_requests_to_transfer_view.xml',
+        'views/mass.xml',
+        'views/donation.xml',
+        'data/product.xml',
         'security/ir.model.access.csv',
         ],
-    'demo': ['donation_mass_demo.xml'],
+    'demo': ['demo/demo_data.xml'],
     'installable': True,
 }
