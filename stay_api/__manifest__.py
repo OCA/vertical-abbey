3c4c155f6ce1a6f7e3b0dcaeb421c39d605e908b
Alexis de Lattre <alexis.delattre@akretion.com>
2025-05-07 22:19:39 +0000

    [IMP] stay and stay_api: add /read, /update and /cancel in API
    Add support for stay tags
    Add stay_notify_user_ids on res.company, used when the stay is not in a group
    Allow to book rooms in draft state
    Direct access button for mass assign wizard
    Restrict rooms to available rooms in mass assign wizard and improve guest_qty
    When archiving a room, the assign lines are removed from timeline view
    Add button to access stay from assign line tree view
    Add a new Room Transition configuration "Night"
    In stay line mass update, allow to filter by date
    Fix layout of stay configuration page
    Switch mail template to noupdate=false via migration script

--- stay_api/__manifest__.py
+++ stay_api/__manifest__.py
@@ -15,12 +15,12 @@
     "external_dependencies": {"python": ["fastapi", "pydantic<2"]},
     "data": [
         "data/res_users.xml",
-        "security/res_groups.xml",
         "security/ir.model.access.csv",
         "wizards/stay_create_partner_view.xml",
         "wizards/res_config_settings_view.xml",
         "views/stay_stay.xml",
         "data/mail_template.xml",
     ],
+    "post_init_hook": "stay_set_uuid",
     "installable": True,
 }
