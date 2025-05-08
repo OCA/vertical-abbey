8ede8ee961b575cba5bccb6f71c1f63bcb99bcc3
Alexis de Lattre <alexis.delattre@akretion.com>
2025-05-08 09:05:18 +0000

    [IMP] stay_api: add phone in controllers and wizards (in addition to mobile)

--- stay_api/post_install.py
+++ stay_api/post_install.py
@@ -2,16 +2,19 @@
 # @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, SUPERUSER_ID
-from uuid import uuid4
 import logging
+from uuid import uuid4
+
+from odoo import SUPERUSER_ID, api
+
 logger = logging.getLogger(__name__)
+
 
 def stay_set_uuid(cr, registry):
     with api.Environment.manage():
         env = api.Environment(cr, SUPERUSER_ID, {})
-        logger.info('Starting to write uuid on stays')
-        stays = env['stay.stay'].search([])
+        logger.info("Starting to write uuid on stays")
+        stays = env["stay.stay"].search([])
         for stay in stays:
-            stay.write({'controller_uuid': str(uuid4())})
-        logger.info('%d stays updated with uuid', len(stays))
+            stay.write({"controller_uuid": str(uuid4())})
+        logger.info("%d stays updated with uuid", len(stays))
--- stay_api/wizards/stay_create_partner.py
+++ stay_api/wizards/stay_create_partner.py
@@ -21,6 +21,7 @@ class StayCreatePartner(models.TransientModel):
         ]
     )
     email = fields.Char(string="E-mail")
+    phone = fields.Char()
     mobile = fields.Char()
     street = fields.Char(string="Address Line 1")
     street2 = fields.Char(string="Address Line 2")
@@ -30,6 +31,9 @@ class StayCreatePartner(models.TransientModel):
     update_partner_id = fields.Many2one("res.partner", string="Partner to Update")
     update_partner_email = fields.Char(
         related="update_partner_id.email", string="Current E-mail"
+    )
+    update_partner_phone = fields.Char(
+        related="update_partner_id.phone", string="Current Phone"
     )
     update_partner_mobile = fields.Char(
         related="update_partner_id.mobile", string="Current Mobile"
@@ -57,6 +61,9 @@ class StayCreatePartner(models.TransientModel):
         store=True,
         string="Update E-mail",
     )
+    update_phone = fields.Boolean(
+        compute="_compute_update_bool", readonly=False, store=True
+    )
     update_mobile = fields.Boolean(
         compute="_compute_update_bool", readonly=False, store=True
     )
@@ -83,6 +90,7 @@ class StayCreatePartner(models.TransientModel):
                 "lastname": stay.controller_lastname,
                 "title": stay.controller_title,
                 "email": stay.controller_email and stay.controller_email.lower(),
+                "phone": stay.controller_phone,
                 "mobile": stay.controller_mobile,
                 "street": stay.controller_street,
                 "street2": stay.controller_street2,
@@ -99,6 +107,7 @@ class StayCreatePartner(models.TransientModel):
         rpo = self.env["res.partner"]
         vals = {
             "email": self.email,
+            "phone": self.phone,
             "mobile": self.mobile,
             "street": self.street,
             "street2": self.street2,
@@ -138,16 +147,20 @@ class StayCreatePartner(models.TransientModel):
     def _compute_update_bool(self):
         for wiz in self:
             update_email = False
+            update_phone = False
             update_mobile = False
             update_address = False
             if wiz.update_partner_id:
                 if wiz.email and wiz.email != wiz.update_partner_id.email:
                     update_email = True
+                if wiz.phone and wiz.phone != wiz.update_partner_id.phone:
+                    update_phone = True
                 if wiz.mobile and wiz.mobile != wiz.update_partner_id.mobile:
                     update_mobile = True
                 if wiz.street and wiz.city and wiz.zip and wiz.country_id:
                     update_address = True
             wiz.update_email = update_email
+            wiz.update_phone = update_phone
             wiz.update_mobile = update_mobile
             wiz.update_address = update_address
 
@@ -156,6 +169,8 @@ class StayCreatePartner(models.TransientModel):
         if not self.update_partner_id:
             raise UserError(_("The partner to update is not set."))
         vals = {}
+        if self.update_phone:
+            vals["phone"] = self.phone
         if self.update_mobile:
             vals["mobile"] = self.mobile
         if self.update_email:
@@ -173,7 +188,9 @@ class StayCreatePartner(models.TransientModel):
         if vals:
             self.update_partner_id.write(vals)
             self.update_partner_id.message_post(
-                body=_("Partner e-mail and/or mobile updated from stay web form.")
+                body=_(
+                    "Partner e-mail and/or phone and/or mobile updated from stay web form."
+                )
             )
         self.stay_id.write({"partner_id": self.update_partner_id.id})
         self.stay_id.message_post(body=_("Partner updated from stay web form."))
