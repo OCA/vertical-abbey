9b4df9b5a2caef69ddc3a765097aceba9885a2d5
Alexis de Lattre <alexis.delattre@akretion.com>
2025-05-22 15:51:57 +0200

    [IMP] stay_api: improve string and pep8 fixes

--- stay_api/models/res_partner_title.py
+++ stay_api/models/res_partner_title.py
@@ -10,10 +10,10 @@ class ResPartnerTitle(models.Model):
 
     # We need to have this code to make the API work for titles
     # created by hand by the user that don't have any XMLID
-    stay_code = fields.Char(copy=False, help="Technical code used by the stay API. Do not modify!")
+    stay_code = fields.Char(
+        copy=False, help="Technical code used by the stay API. Do not modify!"
+    )
 
     _sql_constraints = [
-        (
-            'stay_code_uniq',
-            'unique(stay_code)',
-            'This stay code already exists.')]
+        ("stay_code_uniq", "unique(stay_code)", "This stay code already exists.")
+    ]
