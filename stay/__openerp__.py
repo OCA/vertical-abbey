OCA/donation

stay: don't set refectory when no_meals option active on stay Add tests in stay and donation modules Update demo data

donation/__openerp__.py
@@ -53,4 +53,5 @@ It has been developped by brother Bernard and brother Irenee from Barroux Abbey 
         'product_demo.xml',
         'donation_demo.xml',
         ],
+    'test': ['test/validate.yml'],
 }

OCA/vertical-abbey

stay/__openerp__.py
@@ -54,5 +54,6 @@ Barroux Abbey and by Alexis de Lattre from Akretion.
         'partner_view.xml',
         ],
     'demo': ['stay_demo.xml'],
+    'test': ['test/generate_journal.yml'],
     'installable': True,
 }