OCA/donation

Port to new API Add default donation tax receipt report Small usability enhancements

donation_bank_statement/company.py
@@ -21,13 +21,11 @@
 #
 ##############################################################################
 
-from openerp.osv import orm, fields
+from openerp import models, fields
 
 
-class res_company(orm.Model):
+class ResCompany(models.Model):
     _inherit = 'res.company'
 
-    _columns = {
-        'donation_credit_transfer_journal_id': fields.many2one(
-            'account.journal', 'Donations via Credit Transfer Journal'),
-        }
+    donation_credit_transfer_journal_id = fields.Many2one(
+        'account.journal', string='Donations via Credit Transfer Journal')

OCA/vertical-abbey

mass/company.py
@@ -22,16 +22,14 @@
 #
 ##############################################################################
 
-from openerp.osv import orm, fields
+from openerp import models, fields
 
 
-class res_company(orm.Model):
+class ResCompany(models.Model):
     _inherit = 'res.company'
 
-    _columns = {
-        'mass_validation_account_id': fields.many2one(
-            'account.account', 'Mass Validation Account',
-            domain=[('type', '<>', 'view'), ('type', '<>', 'closed')]),
-        'mass_validation_journal_id': fields.many2one(
-            'account.journal', 'Mass Validation Journal'),
-        }
+    mass_validation_account_id = fields.Many2one(
+        'account.account', string='Mass Validation Account',
+        domain=[('type', '<>', 'view'), ('type', '<>', 'closed')])
+    mass_validation_journal_id = fields.Many2one(
+        'account.journal', string='Mass Validation Journal')